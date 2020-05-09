57b354a92e7cf334a73102b446570eee11b1b860
Brandon <brandf3@uw.edu>
2020-05-09 13:22:17 -0700

    Added FrozenLakeEnvironment, no Rendering

--- frozenLakeEnv.py
+++ frozenLakeEnv.py
@@ -1,4 +1,34 @@
 import environment
+import gym
+import numpy as np
+import random
+from PIL import Image, ImageDraw
+import math
 
 class FrozenLakeEnv(environment.Environment):
-    pass
+
+    def __init__(self):
+        self.env = gym.make('FrozenLake-v0')
+        self.action_size = self.env.action_space.n
+        self.state_size = env.observation_space.n
+        print(self.env.action_space, self.env.observation_space)
+        print(self.action_size, self.state_size)
+
+        self.state = None
+        self.done = None
+        self.total_rewards = None
+
+    def step(self, action):
+        new_state, reward, self.done, info = self.env.step(action)
+        self.state = new_state
+        return reward
+
+    def reset(self):
+        self.state = env.reset()
+        self.done = False
+        self.total_rewards = 0
+
+    def sample_action(self):
+        return self.env.action_space.sample()
+
+    
