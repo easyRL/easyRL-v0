Added action masking: a small speedup for the neural networks, also fixed Q-Table

Agents/adrqn.py
@@ -21,12 +21,6 @@ class ADRQN(drqn.DRQN):
         action = np.argmax(qval)
         return action
 
-    def create_one_hot(self, vector_length, hot_index):
-        output = np.zeros((vector_length))
-        if hot_index != -1:
-            output[hot_index] = 1
-        return output
-
     def predict(self, state, isTarget):
         state, action = state
         stateShape = (1,) + (self.historylength,) + self.state_size
@@ -35,21 +29,22 @@ class ADRQN(drqn.DRQN):
         action = np.reshape(action, actionShape)
 
         if isTarget:
-            result = self.target.predict([state, action])
+            result = self.target.predict([state, action, self.allMask])
         else:
-            result = self.model.predict([state, action])
+            result = self.model.predict([state, action, self.allMask])
         return result
 
     def buildQNetwork(self):
-
         from tensorflow.python.keras.optimizer_v2.adam import Adam
         from tensorflow.keras.models import Model
         from tensorflow.keras.layers import Input, Dense, Conv2D
-        from tensorflow.keras.layers import MaxPool2D, Flatten, TimeDistributed, LSTM, concatenate
+        from tensorflow.keras.layers import Flatten, TimeDistributed, LSTM, concatenate, multiply
 
         input_shape = (self.historylength,) + self.state_size
         inputA = Input(shape=input_shape)
         inputB = Input(shape=(self.historylength, self.action_size))
+        inputC = Input(shape=(self.action_size,))
+
         if len(self.state_size) == 1:
             x = TimeDistributed(Dense(24, activation='relu'))(inputA)
         else:
@@ -67,16 +62,18 @@ class ADRQN(drqn.DRQN):
         z = LSTM(256)(combined)
         z = Dense(10, activation='relu')(z)  # fully connected
         z = Dense(10, activation='relu')(z)
-        outputs = Dense(self.action_size)(z)
+        z = Dense(self.action_size)(z)
+        outputs = multiply([z, inputC])
 
-        inputs = [inputA, inputB]
+        inputs = [inputA, inputB, inputC]
         model = Model(inputs=inputs, outputs=outputs)
         model.compile(loss='mse', optimizer=Adam(lr=0.0001, clipvalue=1))
-        return model, inputs, outputs
+        return model
 
     def calculateTargetValues(self, mini_batch):
         X_train = [np.zeros((self.batch_size,) + (self.historylength,) + self.state_size),
-                   np.zeros((self.batch_size,) + (self.historylength,) + (self.action_size,))]
+                   np.zeros((self.batch_size,) + (self.historylength,) + (self.action_size,)),
+                   np.zeros((self.batch_size,) + (self.action_size,))]
         next_states = [np.zeros((self.batch_size,) + (self.historylength,) + self.state_size),
                    np.zeros((self.batch_size,) + (self.historylength,) + (self.action_size,))]
 
@@ -86,16 +83,18 @@ class ADRQN(drqn.DRQN):
                 next_states[0][index_rep][histInd] = next_state
                 X_train[1][index_rep][histInd] = self.create_one_hot(self.action_size, prevAction)
                 next_states[1][index_rep][histInd] = self.create_one_hot(self.action_size, action)
+            X_train[2][index_rep] = self.create_one_hot(self.action_size, action)
 
-        Y_train = self.model.predict(X_train)
-        qnext = self.target.predict(next_states)
+        Y_train = np.zeros((self.batch_size,) + (self.action_size,))
+        qnext = self.target.predict([next_states, self.allBatchMask])
+        qnext = np.amax(qnext, 1)
 
         for index_rep, history in enumerate(mini_batch):
             prevAction, state, action, reward, next_state, isDone = history[-1]
             if isDone:
                 Y_train[index_rep][action] = reward
             else:
-                Y_train[index_rep][action] = reward + np.amax(qnext[index_rep]) * self.gamma
+                Y_train[index_rep][action] = reward + qnext[index_rep] * self.gamma
         return X_train, Y_train
 
 

Agents/deepQ.py
@@ -16,11 +16,12 @@ class DeepQ(modelFreeAgent.ModelFreeAgent):
         paramLen = len(DeepQ.newParameters)
         super().__init__(*args[:-paramLen])
         self.batch_size, self.memory_size, self.target_update_interval = [int(arg) for arg in args[-paramLen:]]
-        self.model, self.inputs, self.outputs = self.buildQNetwork()
-        self.outputModels = self.buildOutputNetworks(self.inputs, self.outputs)
-        self.target, _, _ = self.buildQNetwork()
+        self.model = self.buildQNetwork()
+        self.target = self.buildQNetwork()
         self.memory = deque(maxlen=self.memory_size)
         self.total_steps = 0
+        self.allMask = np.full((1, self.action_size), 1)
+        self.allBatchMask = np.full((self.batch_size, self.action_size), 1)
 
     def choose_action(self, state):
         qval = self.predict(state, False)
@@ -62,9 +63,9 @@ class DeepQ(modelFreeAgent.ModelFreeAgent):
         shape = (1,) + self.state_size
         state = np.reshape(state, shape)
         if isTarget:
-            result = self.target.predict(state)
+            result = self.target.predict([state, self.allMask])
         else:
-            result = self.model.predict(state)
+            result = self.model.predict([state, self.allMask])
         return result
 
     def update(self):
@@ -73,47 +74,46 @@ class DeepQ(modelFreeAgent.ModelFreeAgent):
     def reset(self):
         pass
 
+    def create_one_hot(self, vector_length, hot_index):
+        output = np.zeros((vector_length))
+        if hot_index != -1:
+            output[hot_index] = 1
+        return output
+
     def buildQNetwork(self):
         from tensorflow.python.keras.optimizer_v2.adam import Adam
         from tensorflow.keras.models import Model
-        from tensorflow.keras.layers import Dense, Input, Flatten
+        from tensorflow.keras.layers import Dense, Input, Flatten, multiply
 
-        inputs = Input(shape=self.state_size)
-        x = Flatten()(inputs)
+        inputA = Input(shape=self.state_size)
+        inputB = Input(shape=(self.action_size,))
+        x = Flatten()(inputA)
         x = Dense(24, input_dim=self.state_size, activation='relu')(x)  # fully connected
         x = Dense(24, activation='relu')(x)
-        outputs = Dense(self.action_size, activation='linear')(x)
-        model = Model(inputs=inputs, outputs=outputs)
+        x = Dense(self.action_size, activation='linear')(x)
+        outputs = multiply([x, inputB])
+        model = Model(inputs=[inputA, inputB], outputs=outputs)
         model.compile(loss='mse', optimizer=Adam(lr=0.001))
-        return model, inputs, outputs
-
-    def buildOutputNetworks(self, inputs, outputs):
-        from tensorflow.python.keras.optimizer_v2.adam import Adam
-        from tensorflow.keras.models import Model
-
-        models = []
-        for index in range(self.outputs.shape[1]):
-            model = Model(inputs=inputs, outputs=outputs[:, index])
-            model.compile(loss='mse', optimizer=Adam(lr=0.001))
-            models.append(model)
-        return models
+        return model
 
     def calculateTargetValues(self, mini_batch):
-        X_train = np.zeros((self.batch_size,) + self.state_size)
+        X_train = [np.zeros((self.batch_size,) + self.state_size), np.zeros((self.batch_size,) + (self.action_size,))]
         next_states = np.zeros((self.batch_size,) + self.state_size)
 
         for index_rep, (state, action, reward, next_state, isDone) in enumerate(mini_batch):
-            X_train[index_rep] = state
+            X_train[0][index_rep] = state
+            X_train[1][index_rep] = self.create_one_hot(self.action_size, action)
             next_states[index_rep] = next_state
 
-        Y_train = self.model.predict(X_train)
-        qnext = self.target.predict(next_states)
+        Y_train = np.zeros((self.batch_size,) + (self.action_size,))
+        qnext = self.target.predict([next_states, self.allBatchMask])
+        qnext = np.amax(qnext, 1)
 
         for index_rep, (state, action, reward, next_state, isDone) in enumerate(mini_batch):
             if isDone:
                 Y_train[index_rep][action] = reward
             else:
-                Y_train[index_rep][action] = reward + np.amax(qnext[index_rep]) * self.gamma
+                Y_train[index_rep][action] = reward + qnext[index_rep] * self.gamma
         return X_train, Y_train
 
     def __deepcopy__(self, memodict={}):

Agents/drqn.py
@@ -26,46 +26,47 @@ class DRQN(deepQ.DeepQ):
         from tensorflow.python.keras.optimizer_v2.adam import Adam
         from tensorflow.keras.models import Model
         from tensorflow.keras.layers import Input, Dense, Conv2D
-        from tensorflow.keras.layers import MaxPool2D, Flatten, TimeDistributed, LSTM
+        from tensorflow.keras.layers import Flatten, TimeDistributed, LSTM, multiply
 
         input_shape = (self.historylength,) + self.state_size
-        inputs = Input(shape=input_shape)
+        inputA = Input(shape=input_shape)
+        inputB = Input(shape=(self.action_size,))
 
         if len(self.state_size) == 1:
-            x = TimeDistributed(Dense(10, input_shape=input_shape, activation='relu'))(inputs)
+            x = TimeDistributed(Dense(10, input_shape=input_shape, activation='relu'))(inputA)
         else:
-            x = TimeDistributed(Conv2D(32, (3,3), activation='relu'))(inputs)
-            x = TimeDistributed(MaxPool2D(pool_size=(2,2)))(x)
-            x = TimeDistributed(Conv2D(64, (3,3), activation='relu'))(x)
-            x = TimeDistributed(MaxPool2D(pool_size=(2,2)))(x)
-
+            x = TimeDistributed(Conv2D(16, 8, strides=4, activation='relu'))(inputA)
+            x = TimeDistributed(Conv2D(32, 4, strides=2, activation='relu'))(x)
         x = TimeDistributed(Flatten())(x)
-        x = LSTM(512)(x)
+        x = LSTM(256)(x)
         x = Dense(10, activation='relu')(x)  # fully connected
         x = Dense(10, activation='relu')(x)
-        outputs = Dense(self.action_size)(x)
-        model = Model(inputs=inputs, outputs=outputs)
+        x = Dense(self.action_size)(x)
+        outputs = multiply([x, inputB])
+        model = Model(inputs=[inputA, inputB], outputs=outputs)
         model.compile(loss='mse', optimizer=Adam(lr=0.0001, clipvalue=1))
-        return model, inputs, outputs
+        return model
 
     def calculateTargetValues(self, mini_batch):
-        X_train = np.zeros((self.batch_size,) + (self.historylength,) + self.state_size)
+        X_train = [np.zeros((self.batch_size,) + (self.historylength,) + self.state_size), np.zeros((self.batch_size,) + (self.action_size,))]
         next_states = np.zeros((self.batch_size,) + (self.historylength,) + self.state_size)
 
         for index_rep, history in enumerate(mini_batch):
             for histInd, (state, action, reward, next_state, isDone) in enumerate(history):
-                X_train[index_rep][histInd] = np.array(state)
+                X_train[0][index_rep][histInd] = state
                 next_states[index_rep][histInd] = np.array(next_state)
+            X_train[1][index_rep] = self.create_one_hot(self.action_size, action)
 
-        Y_train = self.model.predict(X_train)
-        qnext = self.target.predict(next_states)
+        Y_train = np.zeros((self.batch_size,) + (self.action_size,))
+        qnext = self.target.predict([next_states, self.allBatchMask])
+        qnext = np.amax(qnext, 1)
 
         for index_rep, history in enumerate(mini_batch):
             state, action, reward, next_state, isDone = history[-1]
             if isDone:
                 Y_train[index_rep][action] = reward
             else:
-                Y_train[index_rep][action] = reward + np.amax(qnext[index_rep]) * self.gamma
+                Y_train[index_rep][action] = reward + qnext[index_rep] * self.gamma
         return X_train, Y_train
 
     def choose_action(self, state):
@@ -81,9 +82,9 @@ class DRQN(deepQ.DeepQ):
         state = np.reshape(state, shape)
         state = tf.cast(state, dtype=tf.float32)
         if isTarget:
-            result = self.target.predict(state)
+            result = self.target.predict([state, self.allMask])
         else:
-            result = self.model.predict(state)
+            result = self.model.predict([state, self.allMask])
         return result
 
     def sample(self):