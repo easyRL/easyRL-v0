43bbfb2a309d54fc411069e8a3542529fcbda20f
trstew <trstew98@gmail.com>
2021-02-19 14:44:23 -0800

    Switched the transition frame from objects to named tuple.

--- Agents/Collections/TransitionFrame.py
+++ Agents/Collections/TransitionFrame.py
@@ -1,12 +1,4 @@
-class TransitionFrame:
-    def __init__(self, state, action, reward, next_state, is_done):
-        self.state = state
-        self.action = action
-        self.reward = reward
-        self.next_state = next_state
-        self.is_done = is_done
+from collections import namedtuple
 
-class ActionTransitionFrame(TransitionFrame):
-    def __init__(self, prev_action, state, action, reward, next_state, is_done):
-        super().__init__(state, action, reward, next_state, is_done)
-        self.prev_action = prev_action
+TransitionFrame = namedtuple('TransitionFrame', ['state', 'action', 'reward', 'next_state', 'is_done'])
+ActionTransitionFrame = namedtuple('ActionTransitionFrame', ['prev_action', 'state', 'action', 'reward', 'next_state', 'is_done'])
