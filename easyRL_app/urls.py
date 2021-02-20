ebb87c247e83869889a3db9463be03f7a260fe2c
Rashad <rhatch26@gmail.com>
2021-02-20 14:08:07 -0800

    found an error when merging

--- easyRL_app/urls.py
+++ easyRL_app/urls.py
@@ -11,5 +11,4 @@ urlpatterns = [
     path('test/', view.test_data, name='test'),
     path('test_ci/', view.test_create_instance, name='create_instance'),
     path('test_ti/', view.test_terminate_instance, name='terminate_instance'),
-    path('test_rj/', view.test_run_job, name='run_job'),
 ]
