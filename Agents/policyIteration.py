12dabf3df99830100e41b74682c9f4e6623afd82
Shrustishree <64390574+Shrustishree@users.noreply.github.com>
2021-02-21 18:04:15 -0800

    Update policyIteration.py

--- Agents/policyIteration.py
+++ Agents/policyIteration.py
@@ -1,11 +1,15 @@
 from Agents import modelBasedAgent
-
+from Agents import agent
 
 class PolicyIteration(modelBasedAgent.ModelBasedAgent):
+    newParameters = []
+    parameters = modelBasedAgent.ModelBasedAgent.parameters + newParameters
+    
     def __init__(self):
         super().__init__()
         self.value = []
         self.policy = []
+
 
     def update(self):
         super().update()
