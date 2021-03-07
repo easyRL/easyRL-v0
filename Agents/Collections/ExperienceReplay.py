6b239a8083163f2165a12bf30d24bd06a74920e4
Shrustishree <64390574+Shrustishree@users.noreply.github.com>
2021-03-06 22:38:58 -0800

    Update ExperienceReplay.py

--- Agents/Collections/ExperienceReplay.py
+++ Agents/Collections/ExperienceReplay.py
@@ -403,3 +403,30 @@ class PrioritizedReplayBuffer(ReplayBuffer):
         :rtype: int
         """
         return idx + self.max_length - 1
+   
+
+class HER(ReplayBuffer):
+    def __init__(self,N):
+        self.buffer = deque()
+        self.N = N
+        
+    def reset(self):
+        self.buffer = deque()
+        
+    def keep(self,item):
+        self.buffer.append(item)
+        
+    def backward(self):
+
+        new_buffer = copy.deepcopy(self.buffer)
+        num = len(new_buffer)
+        goal = self.buffer[-1][-2][0:self.N]
+        for i in range(num):
+            new_buffer[-1-i][2] = -1.0
+            new_buffer[-1-i][-2][self.N:] = goal
+            new_buffer[-1-i][0][self.N:] = goal
+            new_buffer[-1-i][4] = False
+            if (np.sum(np.abs((new_buffer[-1-i][-2][self.N:] - goal))) == 0):
+                new_buffer[-1-i][2] = 0.0
+                new_buffer[-1-i][4] = True
+        return new_buffer
