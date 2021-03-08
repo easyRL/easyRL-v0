9eaf983c8d1502ad3d318485d68c04a7a195fcc5
egormaks <egor.maksimenka@gmail.com>
2021-03-08 10:58:48 -0800

    Debugging tensor issue

--- Agents/models.py
+++ Agents/models.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 from tensorflow.python.keras.optimizer_v2.adam import Adam
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, Input, Flatten, multiply, Lambda
-
+import sys
 
 class Actor(DeepQ):
   def __init__(self, state_size, action_size, policy_lr):
@@ -18,17 +18,22 @@ class Actor(DeepQ):
     #self.optim = tf.keras.optimizers.Adam(self.policy_lr)
 
   def policy_network(self):
-    inputA = Input(shape=self.state_size)
-    inputA = Flatten()(inputA)
-    model = tf.keras.Sequential([
-            inputA,
-            Dense(32, activation='relu'),
-            Dense(16, activation='relu'),
-            Dense(self.action_size, activation='softmax')
-    ])
-    kl = tf.keras.losses.KLDivergence()
-    model.compile(loss=kl, optimizer=Adam(lr = self.policy_lr))
-    return model
+    try:
+      # inputA = Input(shape=self.state_size)
+      # inputA = Flatten()(inputA)
+      print(type(inputA))
+      model = tf.keras.Sequential([
+              Dense(32, activation='relu', input_shape=(self.state_size,)),
+              Dense(16, activation='relu', input_shape=(self.state_size,)),
+              Dense(self.action_size, activation='softmax', input_shape=(self.state_size,))
+      ])
+      kl = tf.keras.losses.KLDivergence()
+      model.compile(loss=kl, optimizer=Adam(lr = self.policy_lr))
+      return model
+    except:
+      print("\n\n\n")
+      print(sys.exc_info()[0])
+      sys.exit()
 
 class Critic(DeepQ):
   def __init__(self, state_size, action_size, value_lr):
