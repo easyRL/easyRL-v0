0b5b70cf7ab96760faecc6d9dab8ab2f5f49b98a
Shrustishree <64390574+Shrustishree@users.noreply.github.com>
2021-03-10 16:51:59 -0800

    Update adrqn.py

--- Agents/adrqn.py
+++ Agents/adrqn.py
@@ -151,8 +151,8 @@ class ADRQN(drqn.DRQN):
     
     def apply_hindsight(self):
         '''
-        Apply the hindsight replay buffer and check if its an instance if
-        yes add it to memory
+       The hindsight replay buffer method checks for 
+       the instance, if instance found add to the memory
                '''
         if (isinstance(self.memory, ExperienceReplay.HindsightReplayBuffer)):
             self.memory.apply_hindsight()
